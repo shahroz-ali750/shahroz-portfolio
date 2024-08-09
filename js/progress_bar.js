567fb8faa4f0c9396c1ac47f419107bce1d843d2
shahroz <shahroz@gmail.com>
2024-08-09 17:18:14 +0500

    no message

--- js/progress_bar.js
+++ js/progress_bar.js
@@ -1,18 +1,25 @@
-let counter = 0;
-window.addEventListener("DOMContentLoaded",move=() =>{
-    if (counter == 0) {
-        j = 1;
-        let elem = document.querySelector(".progress-done");
-        let width = 50;
-        let main = setInterval(frame,50);
-        function frame() {
-            if (width >= 80) {
-                clearInterval(main);
-            } else {
-                width++;
-                elem.style.width = width + "%";
-                elem.innerHTML = width + "%";
-            }
-        }
+window.addEventListener(
+  "DOMContentLoaded",
+  (move = () => {
+    fillProgressBar("htmlProgress", 85);
+    fillProgressBar("cssProgress", 75);
+    fillProgressBar("jsProgress", 60);
+    fillProgressBar("reactProgress", 50);
+  })
+);
+
+function fillProgressBar(elementId, targetWidth) {
+  let elem = document.getElementById(elementId);
+  let width = 0;
+  let main = setInterval(frame, 50);
+
+  function frame() {
+    if (width >= targetWidth) {
+      clearInterval(main);
+    } else {
+      width++;
+      elem.style.width = width + "%";
+      elem.innerHTML = width + "%";
     }
-});
+  }
+}
